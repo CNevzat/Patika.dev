34971ad26d9eeb826ade3da7a7aa5cce1dc32b32
Nevzatcan Çırpıcıoğlu <ncirpicioglu@gmail.com>
2023-04-09 03:53:21 +0300

    arrays

---
+++ .NET-Core-Yeni-Baslayanlar/arrays/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrays
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			//Dizi Tanımlama
+			string[] renkler = new string[5];
+			string[] hayvanlar = { "Kedi", "Köpek", "Kuş", "Maymun" };
+			int[] dizi;
+			dizi = new int[5];
+			//Dizilere Değer Atama ve Erişim
+			renkler[0] = "Mavi";
+			dizi[3] = 10;
+			Console.WriteLine(hayvanlar[1]);
+			Console.WriteLine(dizi[3]);
+			Console.WriteLine(renkler[0]);
+			// Döngülerler dizi kullanımı
+			// Kalvyeden girilen n tane sayının ortalamasını hesaplayan program Console.Write("Lütfen dizinin eleman sayisini giriniz: ");
+			Console.WriteLine("Lütfen dizinin eleman sayısını giriniz:");
+			int diziUzunlugu = int.Parse(Console.ReadLine());
+			int[] sayiDizisi = new int[diziUzunlugu];
+			for (int i = 0; i < diziUzunlugu; i++)
+			{
+				Console.Write("Lütfen (0). sayısı giriniz: ", i + 1);
+				sayiDizisi[i] = int.Parse(Console.ReadLine());
+			}		
+			int toplam = 0;
+			foreach (var sayi in sayiDizisi)
+			toplam += sayi;
+			Console.WriteLine("Ortalama :" + toplam / diziUzunlugu);
+		}
+	}
+}
