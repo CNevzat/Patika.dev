65539a5edb40f281c652e42c132bc24bbbf68135
Nevzatcan Çırpıcıoğlu <ncirpicioglu@gmail.com>
2023-05-05 23:45:18 +0300

    Insertion_Sort

---
+++ .NET-Core-Yeni-Baslayanlar/Coderbyte/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coderbyte
+{
+	public class Program
+	{
+		public static int CheckNums(int num1, int num2)
+		{
+			if (num2 > num1)
+			{ Console.WriteLine("true"); }
+			else if (num1 > num2)
+			{
+				Console.WriteLine("false");
+			}
+			else { Console.WriteLine("-1"); }
+			return num2;		
+  }
+		static void Main(string[] args)
+		{
+			CheckNums(2,1);
+			Console.ReadKey();
+		}
+	}
+}
