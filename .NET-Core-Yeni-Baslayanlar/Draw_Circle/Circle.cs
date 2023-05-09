363576ec4c409252e1ba5493b85f2c31713efdfc
Nevzatcan Çırpıcıoğlu <ncirpicioglu@gmail.com>
2023-05-09 22:45:36 +0300

    Daire_Ciz

---
+++ .NET-Core-Yeni-Baslayanlar/Draw_Circle/Circle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Draw_Circle
+{
+	public class Circle
+	{
+		public void DrawCircle(int radius)
+		{
+			int centerOfX = 20; // X coordinate of center of circle
+			int centerOfY = 10; // Y coordinate of center of circle
+			 // radius of circle
+
+			// Draw circle
+			for (int y = centerOfY - radius; y <= centerOfY + radius; y++)
+			{
+				for (int x = centerOfX - radius; x <= centerOfX + radius; x++)
+				{
+					double d = Math.Sqrt(Math.Pow(x - centerOfX, 2) + Math.Pow(y - centerOfY, 2));
+					if (d < radius)
+					{
+						Console.Write("*");
+					}
+					else
+					{
+						Console.Write(" ");
+					}
+				}
+				Console.WriteLine();
+			}
+		}
+	}
+}
---
+++ .NET-Core-Yeni-Baslayanlar/Draw_Circle/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Draw_Circle
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			Circle circle = new Circle();
+
+			Console.Write("Çizmek istediğiniz dairenin yarıçapını giriniz: ");
+			int radius = int.Parse(Console.ReadLine());
+
+			circle.DrawCircle(radius);
+
+			Console.ReadKey();
+		}
+	}
+}
